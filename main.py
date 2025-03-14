a6f6947d35a067f2c903b32bc12fc460f232cd05
karthik <you@example.com>
2025-03-14 06:51:35 +0000

    changed main.py file

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app =Flask(__name__)
 
 @app.route('/')
 def home():
-        return "Welcome to python Flask world v1.0" 
+        return "Welcome to python karthik 1.0" 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port=8080, debug=True)
