a4be407d7f90469645dbcfaf954c60cdf9a45940
karthik <you@example.com>
2025-03-15 05:05:26 +0000

    added 3 files

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app =Flask(__name__)
 def home():
         return "Welcome to python karthik 1.0" 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port=8080, debug=True)
+    app.run(host ='0.0.0.0',  debug=True)
